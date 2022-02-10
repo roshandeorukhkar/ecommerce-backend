bd3b10b04ddd1a0ae18b62c129117fb5d180368c
ashish-keasofttech <aashishr01@keasofttech.com>
2022-02-10 18:58:31 +0530

    add app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const braintreeRoutes = require('./routes/braintree');
 const orderRoutes = require('./routes/order');
 const userRoutes = require('./routes/user');
 const storeRoutes = require('./routes/store');
-const manRoutes = require('./routes/man');
+const manufacturersRoutes = require('./routes/manufacturer');
 
 // app
 const app = express();
@@ -45,7 +45,7 @@ app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
 app.use('/api', braintreeRoutes);
 app.use('/api', orderRoutes);
-app.use('/api', manRoutes);
+app.use('/api', manufacturersRoutes);
 app.use('/api', userRoutes);
 app.use('/api', storeRoutes);
 
