b68c7c62b540dd979a85111378a6976c3e6f1d3b
ashish-keasofttech <aashishr01@keasofttech.com>
2022-03-07 12:13:20 +0530

    name changes

--- routes/manufacturer.js
+++ routes/manufacturer.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const { create, productById, read, update, remove, list, updateDelete, updateStatus, changeStatus } = require('../controllers/manufacturer');
+const { create, manufacturerById, read, update, remove, list, updateDelete, updateStatus, changeStatus } = require('../controllers/manufacturer');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 const { maniValidator } = require("../validator");
 
-router.get('/manufacturer/:productId', read);
+router.get('/manufacturer/:manufacturerId', read);
 router.get('/manufacturer', list);
 router.post('/manufacturer/create', maniValidator, create);
-router.delete("/manufacturer/:productId", remove);
-router.put("/manufacturer/:productId", maniValidator, update);
+router.delete("/manufacturer/:manufacturerId", remove);
+router.put("/manufacturer/:manufacturerId", maniValidator, update);
 
 
-router.post("/manufacturer/delete/:productId", updateDelete);
+router.post("/manufacturer/delete/:manufacturerId", updateDelete);
 
-router.post("/manufacturer/status/:productId", updateStatus);
+router.post("/manufacturer/status/:manufacturerId", updateStatus);
 
-router.post("/manufacturer/statusChange/:productId", changeStatus);
+router.post("/manufacturer/statusChange/:manufacturerId", changeStatus);
 
 
-router.param('productId', productById);
+router.param('manufacturerId', manufacturerById);
+
+//router.param('productId', manufacturerById);
 //router.param('userId', userById);
 
 module.exports = router;
