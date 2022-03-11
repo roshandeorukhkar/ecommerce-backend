71e952341d59e84e7605f8748b4fd48275e4e969
ashish-keasofttech <aashishr01@keasofttech.com>
2022-03-11 14:07:39 +0530

    add category

--- models/category/category.js
+++ models/category/category.js
@@ -5,7 +5,7 @@ const categorySchema = new mongoose.Schema(
         name: {
             type: String,
             trim: true,
-            required: true,
+           // required: true,
             maxlength: 32,
         },
         description: {
@@ -20,7 +20,7 @@ const categorySchema = new mongoose.Schema(
             type : Date
         },
         deletedAt :{
-            require : true,
+            //require : true,
             type : Date,
         },
         status: {
