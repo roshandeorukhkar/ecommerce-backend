7a16d64abff9f128d1155d2b3e55c574f7b27ca7
ashish-keasofttech <aashishr01@keasofttech.com>
2022-03-19 11:49:45 +0530

    add product:gaurav

--- routes/product.js
+++ routes/product.js
@@ -1,24 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-    create,
-    productById,
-    read,
-    remove,
-    update,
-    list,
-    listRelated,
-    listCategories,
-    listBySearch,
-    photo,
-    listSearch
-} = require("../controllers/product");
+const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch, photo, listSearch } = require("../controllers/product");
 const { requireSignin, isAuth } = require("../controllers/auth");
 const { userById } = require("../controllers/customer");
 
 router.get("/product/:productId", read);
-router.post("/product/create/:userId", requireSignin, isAuth, create);
+router.post("/product/create/", create);
 router.delete(
     "/product/:productId/:userId",
     requireSignin,
